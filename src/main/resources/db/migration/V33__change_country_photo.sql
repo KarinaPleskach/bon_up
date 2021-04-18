ef908896abbb44f137290066beeeb11105155a1b
Karina Pleskach <karinapleskach@mail.ru>
2021-04-18 13:52:47 +0300

    commit

---
+++ src/main/resources/db/migration/V33__change_country_photo.sql
@@ -0,0 +1,3 @@
+update country set photo_id = 5 where language_key = 'country.BYN';
+update country set photo_id = 6 where language_key = 'country.RUB';
+update country set photo_id = 7 where language_key = 'country.USD';
