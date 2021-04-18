10ef605a5359d561900e2c5399ea80e477d8d134
Karina Pleskach <karinapleskach@mail.ru>
2021-04-18 13:07:53 +0300

    commit

--- src/main/java/com/bsuir/diploma/bonup/controller/additional/CountryController.java
+++ src/main/java/com/bsuir/diploma/bonup/controller/additional/CountryController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,7 +25,7 @@ public class CountryController {
     @Autowired
     private TranslationService translationService;
 
-    @PostMapping("/{lang}/country/names")
+    @GetMapping("/{lang}/country/names")
     @ResponseBody
     public ResponseEntity<ResponseWithIdStringDtos> countries(@PathVariable("lang") String lang) {
         List<IdStringDto> list = countryService.getAllNamesWithId(lang);
