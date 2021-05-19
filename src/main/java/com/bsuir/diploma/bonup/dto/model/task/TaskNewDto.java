5808ddd008758827c260f89b6114442fe2aa9040
Karina Pleskach <karinapleskach@mail.ru>
2021-05-19 21:01:46 +0300

    m

---
+++ src/main/java/com/bsuir/diploma/bonup/dto/model/task/TaskNewDto.java
@@ -0,0 +1,23 @@
+package com.bsuir.diploma.bonup.dto.model.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskNewDto {
+    private String title;
+    private String descriptionText;
+    private Long categoryId;
+    private String token;
+    private Long organizationId;
+    private Integer bonusesCount;
+    private Integer allowedCount;
+    private Double startDateTimestamp;
+    private Double endDateTimestamp;
+    private Long photoId;
+}
