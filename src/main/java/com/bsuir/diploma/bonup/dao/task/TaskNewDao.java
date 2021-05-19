05ebe05920e162a1b3a934ea37357e575b4d43ce
Karina Pleskach <karinapleskach@mail.ru>
2021-05-19 21:07:02 +0300

    m

---
+++ src/main/java/com/bsuir/diploma/bonup/dao/task/TaskNewDao.java
@@ -0,0 +1,19 @@
+package com.bsuir.diploma.bonup.dao.task;
+
+import com.bsuir.diploma.bonup.model.organization.Organization;
+import com.bsuir.diploma.bonup.model.organization.OrganizationNew;
+import com.bsuir.diploma.bonup.model.task.Task;
+import com.bsuir.diploma.bonup.model.task.TaskNew;
+import com.bsuir.diploma.bonup.model.task.additional.Category;
+import com.bsuir.diploma.bonup.model.task.additional.Type;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskNewDao extends JpaRepository<TaskNew, Long> {
+    Optional<TaskNew> findByOrganizationNewAndId(OrganizationNew organizationNew, Long id);
+    List<TaskNew> findAllByOrganizationNew(OrganizationNew organizationNew);
+}
